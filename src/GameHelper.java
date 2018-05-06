aa2ffe8c7fecb8e1d4d950a42fa781e72ad75d5d
USSRfantom <dimkakrohin@mail.ru>
2018-05-06 15:13:46 +0300

    Добавляем класс gameHelper чтобы пользователи могли воодить варианты с клавиатуры

---
+++ src/GameHelper.java
@@ -0,0 +1,23 @@
+import javax.imageio.IIOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class GameHelper {
+
+    public String getUserInput (String prompt)
+    {
+        String inputLine = null;
+        System.out.print(prompt + " ");
+        try {
+            BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+            inputLine = is.readLine();
+            if (inputLine.length() == 0)
+                return null;
+        }
+        catch (java.io.IOException e)
+        {
+            System.out.println( "IOException: " + e);
+        }
+        return inputLine;
+    }
+}
